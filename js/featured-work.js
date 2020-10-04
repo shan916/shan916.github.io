48de894e1a1a122a037ab880af8debfae9d6d23c
shan <shan916@icloud.com>
2020-10-03 18:26:18 -0700

    initial commit

---
+++ js/featured-work.js
@@ -0,0 +1,15 @@
+(function() {
+	var app = angular.module('featured-work', []);
+
+	app.controller('FeaturedWorkController', function() {
+		this.projects = apps;
+	});
+
+	var apps = [ 
+	{
+		name: 'Cashculator',
+		imageUrl: 'images/cashculator.png',
+		storeUrl: 'https://itunes.apple.com/us/app/cashculator-cash-counter/id574166464?mt=8'
+	}
+	];
+})();
