d445c8a598ce0c25d6dc3119cd6b69a3101b0e39
shan <shan916@icloud.com>
2020-10-03 18:39:21 -0700

    remove unused files

--- js/featured-work.js
+++ js/featured-work.js
@@ -1,15 +1,15 @@
-(function() {
+(function () {
 	var app = angular.module('featured-work', []);
 
-	app.controller('FeaturedWorkController', function() {
+	app.controller('FeaturedWorkController', function () {
 		this.projects = apps;
 	});
 
-	var apps = [ 
-	{
-		name: 'Cashculator',
-		imageUrl: 'images/cashculator.png',
-		storeUrl: 'https://itunes.apple.com/us/app/cashculator-cash-counter/id574166464?mt=8'
-	}
+	var apps = [
+		{
+			name: 'Cashculator',
+			imageUrl: 'images/cashculator.png',
+			storeUrl: 'https://itunes.apple.com/us/app/cashculator-cash-counter/id574166464?mt=8'
+		}
 	];
 })();
